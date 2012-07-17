407e04644cad0a137fc011e39896dbd959637fb5
upwards <upwards@yandex.ru>
2012-07-17 14:10:55 +0300

    fixed multi selects

--- jquery.touch.js
+++ jquery.touch.js
@@ -30,115 +30,145 @@
   jQuery.fn.extend({
     // Bind an event handler to the "touchstart" JavaScript event.
     touchstart: function(fn) {
-      var that = this[0];
-      that.addEventListener('touchstart', function(event) {
-        fn.call(that);
-      });
+      $(this).each(function() {
+        if (this == undefined)
+          return;
+        
+        var that = this;
+        that.addEventListener('touchstart', function(event) {
+          fn.call(that);
+        });
+      })
 
       return this;
     },
     // Bind an event handler to the "touchend" JavaScript event.
     touchend: function(fn) {
-      var that = this[0];
-      that.addEventListener('touchend', function(event) {
-        fn.call(that);
-      });
+      $(this).each(function() {
+        if (this == undefined)
+          return;
+        
+        var that = this;
+        that.addEventListener('touchend', function(event) {
+          fn.call(that);
+        }, false);
+      })
 
       return this;
     },
     // Bind an event handler to the "touchmove" JavaScript event.
     touchmove: function(fn) {
-      var that = this[0];
-
-      that.addEventListener('touchstart', function(event) {
-        event.preventDefault();
-      }, false);
-
-      that.addEventListener('touchmove', function(event) {
-        var x = event.touches[0].pageX
-          , y = event.touches[0].pageY;
+      $(this).each(function() {
+        if (this == undefined)
+          return;
         
-        event.toucheX = x - 100;
-        event.toucheY = y - 50;
-        
-        fn.call(that, event);
-      }, false);
+        var that = this;
+
+        that.addEventListener('touchstart', function(event) {
+          event.preventDefault();
+        }, false);
+
+        that.addEventListener('touchmove', function(event) {
+          var x = event.touches[0].pageX
+            , y = event.touches[0].pageY;
+          
+          event.toucheX = x - 100;
+          event.toucheY = y - 50;
+          
+          fn.call(that, event);
+        }, false);
+      })
 
       return this;
     },
     // Bind an event handler to the "touchup" JavaScript event.
     touchup: function(fn) {
-      var that = this[0]
-        , $that = this
-        , touchX = null
-        , touchY = null;
+      $(this).each(function() {
+        if (this == undefined)
+          return;
 
-      that.addEventListener('touchstart', function(event) {
-        event.preventDefault();
-        
-        touchX = null;
-        touchY = null;
-      }, false);
+        var that = this
+          , $that = $(this)
+          , touchX = null
+          , touchY = null;
+        that.addEventListener('touchstart', function(event) {
+          event.preventDefault();
+          
+          touchX = null;
+          touchY = null;
+        }, false);
 
-      that.addEventListener('touchmove', function(event) {
-        touchX = event.touches[0].pageX;
-        touchY = event.touches[0].pageY;
-      }, false);
+        that.addEventListener('touchmove', function(event) {
+          touchX = event.touches[0].pageX;
+          touchY = event.touches[0].pageY;
+        }, false);
 
-      that.addEventListener('touchend', function(event) {
-        event.preventDefault();
+        that.addEventListener('touchend', function(event) {
+          event.preventDefault();
 
-        var startX = $that.offset().left
-          , startY = $that.offset().top
-          , endX   = $that.outerWidth() + startX
-          , endY   = $that.outerHeight() + startY;
+          var startX = $that.offset().left
+            , startY = $that.offset().top
+            , endX   = $that.outerWidth() + startX
+            , endY   = $that.outerHeight() + startY;
 
-        var isXY = ((touchX > startX) && (touchX < endX)) &&
-                    ((touchY > startY) && (touchY < endY));
-        var isNotMove = !(touchX && touchY);
-        if (isXY || isNotMove)
-          fn.call(that, event);
-      }, false);
+          var isXY = ((touchX > startX) && (touchX < endX)) &&
+                      ((touchY > startY) && (touchY < endY));
+          var isNotMove = !(touchX && touchY);
+          if (isXY || isNotMove) {
+            fn.call(that, event);            
+          }
+        }, false);
+      })
 
       return this;
     },
     // Bind an event handler to the "touchleave" JavaScript event.
     touchleave: function(fn) {
-      var that = this[0]
-        , $that = this
-        , isRun = true;
+      $(this).each(function() {
+        if (this == undefined)
+          return;
 
-      that.addEventListener('touchstart', function(event) {
-        event.preventDefault();
-      }, false);
+        var that = this
+          , $that = $(this)
+          , isRun = true;
 
-      that.addEventListener('touchmove', function(event) {
-        if (isTouchStateLeave($that)) {
-          if (isRun) fn.call(that, event);
-          isRun = false;
-        } else 
-          isRun = true;
-      }, false);
+        that.addEventListener('touchstart', function(event) {
+          event.preventDefault();
+        }, false);
+
+        that.addEventListener('touchmove', function(event) {
+          if (isTouchStateLeave($that)) {
+            if (isRun) fn.call(that, event);
+            isRun = false;
+          } else 
+            isRun = true;
+        }, false);
+      })
 
       return this;
     },
     // Bind an event handler to the "touchenter" JavaScript event.
     touchenter: function(fn) {
-      var that = this[0]
-        , $that = this
-        , isRun = false;
+      $(this).each(function() {
+        if (this == undefined)
+          return;
 
-      that.addEventListener('touchstart', function(event) {
-        event.preventDefault();
-      }, false);
+        var that = this
+          , $that = $(this)
+          , isRun = false;
 
-      that.addEventListener('touchmove', function(event) {
-        if (!isTouchStateLeave($that)) {
-          if (isRun) fn.call(that, event);
-    	  isRun = false;
-        } else
-    	  isRun = true;
-      }, false);
+        that.addEventListener('touchstart', function(event) {
+          event.preventDefault();
+        }, false);
+
+        that.addEventListener('touchmove', function(event) {
+          if (!isTouchStateLeave($that)) {
+            if (isRun) fn.call(that, event);
+            isRun = false;
+          } else
+            isRun = true;
+        }, false);
+      })
 
       return this;
     }
